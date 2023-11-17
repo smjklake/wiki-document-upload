9afbc3635aba2d705940debd94f0bb43bedaa49c
Spencer Jones <sjones@smrising.com>
2023-11-17 11:27:25 -0700

    add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import figlet from "figlet";
+#! /usr/bin/env node
 
-console.log(figlet.textSync("Wiki Upload"));
+import { Command } from 'commander';
+import figlet from 'figlet';
+
+const program = new Command();
+
+console.log(figlet.textSync('Wiki Upload'));
+
+program.version('1.0.0')
+  .description('A CLI to convert and upload documents to a Wiki')
+  .parse(process.argv);
+
+const options = program.opts();
